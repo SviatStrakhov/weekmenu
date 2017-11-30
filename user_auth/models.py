a933e5c994fa82ca7926da5c085c016428f52861
Svo Hurts <svoharts@gmail.com>
2017-11-30 11:07:18 +0200

    added user profile

--- user_auth/models.py
+++ user_auth/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User)
+
+    class Meta(object):
+        verbose_name = 'User Profile'
+
+    mobile_phone = models.CharField(
+        max_length=12,
+        blank=True,
+        verbose_name='Mobile Phone',
+        default=''
+    )
+
+    def __str__(self):
+        return f'{self.user.username}'
--- weekmenu/urls.py
+++ weekmenu/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.contrib.auth.decorators import login_required
 from w_menu.views import homepage, ProductsView, ShopingListView, ProductCreate
 from django.contrib.auth import views as auth_views
-from django.views.generic.base import RedirectView
+from django.views.generic.base import RedirectView, TemplateView
 urlpatterns = [
 
     url(r'^$', homepage, name='home'),
@@ -27,6 +27,7 @@ urlpatterns = [
     url(r'^products/add_product/$', ProductCreate.as_view(), name='add_product'),
 
     # related urls
+    url(r'^users/profile/$', login_required(TemplateView.as_view(template_name='registration/profile.html')), name='profile'),
     url(r'^users/logout/$', auth_views.logout, kwargs={'next_page': 'home'}, name='auth_logout'),
     url(r'^register/complete/$', RedirectView.as_view(pattern_name='home'), name='registration_complete'),
     url(r'^users/', include('registration.backends.simple.urls', namespace='users')),
